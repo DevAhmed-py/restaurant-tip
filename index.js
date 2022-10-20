71170f3f2bad6785f2f8e00debfa0c6a0271775d
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-10-20 21:47:20 +0200

    commit

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function update() {
     let yourBill = document.getElementById("yourBill").value
-    let tipInput = document.getElementById("tipInput").value
-    let splitInput = document.getElementById("splitInput").value
+    let tipPercent = document.getElementById("tipInput").value
+    let splitNumber = document.getElementById("splitInput").value
+    
+    let tipValue = yourBill * (tipPercent / 100)
+    let tipEach = tipValue / splitNumber
+    let newBillEach = (yourBill + tipValue) / splitNumber
+
+    document.getElementById("tipPercent").textContent = tipPercent + "%"
+    document.getElementById("tipValue").textContent = `€ ${tipValue}`
 }
 
 let container = document.getElementById("container")
 container.addEventListener("input", update)
+
